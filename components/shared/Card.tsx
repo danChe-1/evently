49ad5b2dff8156f19d1ce0dca1073153a203f52c
Daniel <dan.cherkasky@gmail.com>
2024-07-15 10:34:41 +0300

    stripe integration

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -38,9 +38,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           <DeleteConfirmation eventId={event._id} />
         </div>
       )}
-      <Link
-        href={`/events/${event._id}`}
-        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
+      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         {!hidePrice && (
           <div className="flex gap-2">
             <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-600">
@@ -54,16 +52,16 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           </div>
         )}
         <p className="p-medium-16 md:p-medium-18 text-gray-500">{formatDateTime(event.startDateTime).dateTime}</p>
-        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
+        <Link href={`/events/${event._id}`}>
+          <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
+        </Link>
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-gray-600">
             {event.organizer.firstName} {event.organizer.lastName}
           </p>
           {hasOrderLink && (
-            <Link
-              href={`/orders?eventId=${event._id}`}
-              className="flex gap-2">
+            <div className="flex gap-2">
               <p className="text-primary-500">Order Details</p>
               <Image
                 src="/assets/icons/arrow.svg"
@@ -71,10 +69,10 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                 width={10}
                 height={10}
               />
-            </Link>
+            </div>
           )}
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
